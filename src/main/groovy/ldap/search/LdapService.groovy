27d42a7ca9fb883bcf257384ad43ed3b90e36638
truemate <truemate@test.com>
2015-03-22 22:17:47 +1100

    - Implement limit for the result set returned by query to LDAP.
    See the following useful discussion for more details about implementation and why it was done this way:
    http://forum.spring.io/forum/spring-projects/data/ldap/18864-sizelimit
    
    Additional proof that it is impossible to achieve setting limit on the client side another way (it is impossible to avoid SizeLimitExceededException):
    http://stackoverflow.com/questions/16734521/spring-ldap-error-code-4-sizelimit-exceeded-when-setting-countlimit-to-1

--- src/main/groovy/ldap/search/LdapService.groovy
+++ src/main/groovy/ldap/search/LdapService.groovy
@@ -3,13 +3,17 @@ package ldap.search
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.ldap.NamingException
 import org.springframework.ldap.core.AttributesMapper
+import org.springframework.ldap.core.AttributesMapperCallbackHandler
+import org.springframework.ldap.core.CollectingNameClassPairCallbackHandler
 import org.springframework.ldap.core.LdapTemplate
 import org.springframework.ldap.filter.AndFilter
 import org.springframework.ldap.filter.EqualsFilter
 import org.springframework.ldap.filter.LikeFilter
 import org.springframework.stereotype.Service
 
+import javax.naming.SizeLimitExceededException
 import javax.naming.directory.Attributes
+import javax.naming.directory.SearchControls
 
 @Service
 class LdapService {
@@ -22,12 +26,25 @@ class LdapService {
         filter.and(new EqualsFilter("objectClass", "person"))
         filter.and(new LikeFilter("cn", "*${name}*"))
         println filter.encode()
-        ldapTemplate.search("",
-                filter.encode(),
-                new AttributesMapper() {
-                    public Object mapFromAttributes(Attributes attrs) throws NamingException {
-                        return attrs.get("cn").get()
-                    }
-                })
+
+        def searchPersonLdapAttributeMapper = new AttributesMapper() {
+            public Object mapFromAttributes(Attributes attrs) throws NamingException {
+                return attrs.get("cn").get()
+            }
+        }
+
+        def searchControls = new SearchControls()
+        searchControls.countLimit = 5
+        searchControls.timeLimit = 0
+        searchControls.searchScope = SearchControls.SUBTREE_SCOPE
+        
+        CollectingNameClassPairCallbackHandler handler = new AttributesMapperCallbackHandler(searchPersonLdapAttributeMapper);
+        try {
+            ldapTemplate.search("",filter.encode(),searchControls, handler);
+        } catch (ex) {
+            // safely ignore. Spring LDAP handles this exception different to SUNs implementation.
+            println "SizeLimitExceededException caught in  PersonLdapDAOImpl.findUserByName"
+        }
+        handler.list
     }
 }
